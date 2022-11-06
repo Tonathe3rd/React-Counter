4fcfc6f1ecef436fac47c55fc57c687edc6bc4b1
Tonathe3rd <get2adeshola@gmail.com>
2022-11-06 22:13:52 +0100

    new commit

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -7,7 +7,8 @@ export const NotFound = () => {
       <div className="four0fourpage">
         <div className="four0four"></div>
         <h1>OOPS!!!</h1>
-        <p>We can't seem to find the page you're looking for</p>
+        <p>We can't seem to find the page you're looking for <br/>
+        Make sure that you have the correct URL and that the file exists.</p>
         <Link className="gohome" to="/">Go Home</Link>
       </div>
     </main>
